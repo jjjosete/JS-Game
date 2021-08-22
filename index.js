ba5534eb49592d21114b8d358ccb96dfb3cfa1a2
jjjosete <jfernan0001@gmail.com>
2021-08-22 17:50:05 +0200

    Primer commit

---
+++ index.js
@@ -0,0 +1,64 @@
+const ARROW_CODES = {
+    37: 'left',
+    38: 'up',
+    39: 'right'
+}
+
+const arrows = trackKeys(ARROW_CODES);
+function trackKeys (keyCodes) {
+    let pressedKeys = {};
+    function handler (event) {
+        
+        if (keyCodes.hasOwnProperty(event.keyCode)){
+            let downPressed = event.type === 'keydown';
+            pressedKeys[keyCodes[event.keyCode]] = downPressed; 
+        }
+        
+    }
+    addEventListener('keydown', handler)
+    addEventListener('keyup', handler)
+
+    return pressedKeys;
+}
+
+function runAnimation (frameFunction) {
+    let lastTime = null;
+    function frame (time) {
+        let stop = false;
+        if (lastTime !== null){
+            let timeStep = Math.min(time - lastTime, 100) / 1000
+            stop = frameFunction(timeStep) === false;
+        }
+        lastTime = time;
+        if (!stop) requestAnimationFrame(frame);
+    }
+    requestAnimationFrame(frame);
+}
+function runLevel(level, Display, callback){
+    let display = new Display(document.body, level);
+    runAnimation(function(step){
+        level.animate(step, arrows)
+        display.drawFrame();
+        if (level.isFinished()){
+            display.clear();
+            if (callback) callback(level.status);
+            return false;
+        }
+    }) 
+}
+
+function runGame (level, Display) {
+    let levelObject;
+    try {
+        levelObject = new Level(GAME_LEVELS);
+    } catch (error){
+        return alert(error.message)
+    }
+
+    runLevel(levelObject, Display, status => {
+        if (status === 'lost') console.log('Has perdido');
+        else console.log('Has ganado!!!')
+    });
+}
+
+runGame(GAME_LEVELS, DOMDisplay);
---
+++ lava.js
@@ -0,0 +1,21 @@
+function Lava (inititalPosition, characterType) {
+    this.position = inititalPosition;
+    this.size = new Vector(1,1);
+
+    if (characterType === '=') this.speed = new Vector (2 ,0);
+    else if (characterType === '|') this.speed = new Vector (0 ,2);
+    else if (characterType === 'v') {
+        this.speed = new Vector (0, 3);
+        this.respawnPosition = inititalPosition;
+    }
+}
+
+Lava.prototype.type = 'lava'
+
+Lava.prototype.act = function (step, level){
+    let newPosition = this.position.plus(this.speed.times(step));
+
+    if (!level.obstacleAt(newPosition, this.size)) this.position = newPosition;
+    else if (this.respawnPosition) this.position = this.respawnPosition;
+    else this.speed = this.speed.times(-1);
+}
